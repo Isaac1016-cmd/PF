2a052a3df817ef114189ae760d9dce292201d3cd
Emmanuel Jimenez <Ejim984@outlook.com>
2021-06-01 17:45:27 -0700

    Label Admin

--- PF/Admin.Designer.cs
+++ PF/Admin.Designer.cs
@@ -49,6 +49,7 @@ namespace PF
             this.lblf = new System.Windows.Forms.Label();
             this.lblhor = new System.Windows.Forms.Label();
             this.Timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
             this.pnmenad.SuspendLayout();
             this.pnlog.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -266,6 +267,7 @@ namespace PF
             // pnconten
             // 
             this.pnconten.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(221)))), ((int)(((byte)(191)))));
+            this.pnconten.Controls.Add(this.label1);
             this.pnconten.Controls.Add(this.lblf);
             this.pnconten.Controls.Add(this.lblhor);
             this.pnconten.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -299,6 +301,15 @@ namespace PF
             // Timer1
             // 
             this.Timer1.Tick += new System.EventHandler(this.Timer1_Tick_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(188, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(205, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Hola lo edito emmanuel";
             // 
             // Admin
             // 
@@ -348,5 +359,6 @@ namespace PF
         private System.Windows.Forms.Label lblf;
         private System.Windows.Forms.Label lblhor;
         private System.Windows.Forms.Timer Timer1;
+        private System.Windows.Forms.Label label1;
     }
 }
