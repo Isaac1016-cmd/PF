e4ef11961d4276fc44321a00dd70bdb53ab467bb
EmmanuelJimenez99 <emmanuel.jimenez18@tectijuana.edu.mx>
2021-06-01 18:28:29 -0700

    Labels Eliminados

--- PF/Admin.Designer.cs
+++ PF/Admin.Designer.cs
@@ -46,12 +46,9 @@ namespace PF
             this.btnclo = new System.Windows.Forms.Button();
             this.lbltil = new System.Windows.Forms.Label();
             this.pnconten = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
             this.lblf = new System.Windows.Forms.Label();
             this.lblhor = new System.Windows.Forms.Label();
             this.Timer1 = new System.Windows.Forms.Timer(this.components);
-            this.label3 = new System.Windows.Forms.Label();
             this.pnmenad.SuspendLayout();
             this.pnlog.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -269,9 +266,6 @@ namespace PF
             // pnconten
             // 
             this.pnconten.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(221)))), ((int)(((byte)(191)))));
-            this.pnconten.Controls.Add(this.label3);
-            this.pnconten.Controls.Add(this.label2);
-            this.pnconten.Controls.Add(this.label1);
             this.pnconten.Controls.Add(this.lblf);
             this.pnconten.Controls.Add(this.lblhor);
             this.pnconten.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -279,24 +273,6 @@ namespace PF
             this.pnconten.Name = "pnconten";
             this.pnconten.Size = new System.Drawing.Size(780, 390);
             this.pnconten.TabIndex = 2;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(188, 78);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(222, 18);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "HOLA LO EDITE YO ISAAC";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(188, 56);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(205, 18);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Hola lo edito emmanuel";
             // 
             // lblf
             // 
@@ -323,15 +299,6 @@ namespace PF
             // Timer1
             // 
             this.Timer1.Tick += new System.EventHandler(this.Timer1_Tick_1);
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(188, 100);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(278, 18);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "ESTE TAMBIEN FUE MODIFICADO";
             // 
             // Admin
             // 
@@ -381,8 +348,5 @@ namespace PF
         private System.Windows.Forms.Label lblf;
         private System.Windows.Forms.Label lblhor;
         private System.Windows.Forms.Timer Timer1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
     }
 }
